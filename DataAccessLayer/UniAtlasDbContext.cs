f65da9972867d6c9f69cc0d1e0872d3ded9fdefc
Hilmi Sühan Akar <61107806+hilmisuhanakar@users.noreply.github.com>
2021-07-01 23:16:13 +0300

    Update UniAtlasDbContext.cs

--- DataAccessLayer/UniAtlasDbContext.cs
+++ DataAccessLayer/UniAtlasDbContext.cs
@@ -10,7 +10,7 @@ namespace DataAccessLayer
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=tcp:uniatlas.database.windows.net,1433;Initial Catalog=UniAtlas;Persist Security Info=False;User ID=o20172013043;Password=BarBarya+627.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            optionsBuilder.UseSqlServer("");
         }
 
         public DbSet<UniDetail> UniDetails { get; set; }
